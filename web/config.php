ecb4a077adf36b7aa7cebb65715b4fc48d2f4cd2
Thetys <nicolas.gaye@gmail.com>
2017-07-21 14:42:35 +0200

    Externalise le style dans des fichiers css

--- web/config.php
+++ web/config.php
@@ -13,6 +13,7 @@
 if (!isset($_SERVER['HTTP_HOST'])) {
     exit("This script cannot be run from the CLI. Run it from a browser.\n");
 }
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
